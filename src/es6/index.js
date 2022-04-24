a61e86802531f3138f168e01469f0d53d9d3700a
missaelcv <missaelcv0220@gmail.com>
2022-04-23 22:25:30 -0400

    Archivo.js

---
+++ src/es6/index.js
@@ -0,0 +1,19 @@
+//Crear funcion 
+function newFunction( name = 'Missael', age='32', country = "RD"){
+    console.log(name, age, country);
+}
+
+newFunction();
+newFunction('Maileny', '24', 'brasil');
+
+let hello = 'Hello';
+let world = "World";
+let nuevoes6 = "nuevoES6"
+let epicPhase = hello + ' ' + world + ' ' + nuevoes6;
+console.log(epicPhase);
+
+//Correr bloques de codigos/ instalar extension code Runner
+//Forma ES6
+
+let epicPhase2 = `${hello} ${world} ${nuevoes6}`;
+console.log(epicPhase2);
