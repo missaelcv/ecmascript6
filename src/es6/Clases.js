ec5817fc2da5d635fe3a7a1714be2039a1a44a8c
missaelcv <missaelcv0220@gmail.com>
2022-04-27 17:32:13 -0400

    Clase y modulos

---
+++ src/es6/Clases.js
@@ -0,0 +1,18 @@
+class calculator {
+
+        //Construtor
+        constructor(){
+            this.valueA = 0; 
+            this.valueB = 0;
+        }
+
+       // Funcion Suma
+        sum(valueA,valueB) {
+            this.valueA = valueA;
+            this.valueB = valueB;
+            return this.valueA + this.valueB;
+        }
+}
+        const calc = new calculator();
+
+        console.log(calc.sum(2,2));
