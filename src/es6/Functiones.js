b150c7776d9522f69a82ad3e90d3e9559ff7bc74
missaelcv <missaelcv0220@gmail.com>
2022-04-26 23:27:34 -0400

    Todas las funciones flecha

--- src/es6/Functiones.js
+++ src/es6/Functiones.js
@@ -11,3 +11,29 @@ obj2 = { name, age};
 console.log(obj2);
 
 //Funciones Tipo Flecha
+const names = [ 
+     { name: 'Missael', age: 24},
+     { name: 'Maileny', age: 21}
+    ]
+     //Obtener el valor individuar 
+    
+      //sin la funciones Flecha
+     let list0fNames = name.map(function (item) {
+         console.log(item.name);
+     })
+
+        //con la funciones Flecha
+        let list0fNames2 = name.map(item => console.log(item.name));
+
+         //con la funciones Flecha #2
+        const list0fNames3 = ( name, age, country) => {
+            //...;
+        }
+
+         //con la funciones Flecha #3
+        const list0fNames4 = name => {
+            //...
+        }
+
+         //con la funciones Flecha #4 Retorna un valor simplicado
+        const square = num => num * num;
