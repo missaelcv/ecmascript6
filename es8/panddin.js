350a78107d2ac2b4bb28a2686884e001b59cafa6
missaelcv <missaelcv0220@gmail.com>
2022-04-28 18:12:30 -0400

    panStard, PanEnd

---
+++ es8/panddin.js
@@ -0,0 +1,9 @@
+const string = 'Hola';
+console.log(string.padStart(7,'ho'))
+
+console.log(string.padEnd(12,' ----'))
+console.log('foof'.padEnd(12,'   -----'))
+
+const obj = { 
+    name: 'missael',
+}
