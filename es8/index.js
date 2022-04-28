5d08458fde752c5b11b0233b57c8d863e9045334
missaelcv <missaelcv0220@gmail.com>
2022-04-28 18:06:02 -0400

    tamaños de arreglos

---
+++ es8/index.js
@@ -0,0 +1,19 @@
+const data = {
+    frontend: 'Missael',
+    backend: 'Alber',
+    desing:  'maileny'
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+const data1 = {
+    frontend: 'Missael',
+    backend: 'Alber',
+    desing:  'maileny'
+}
+
+const value = Object.values(data1);
+console.log(value)
+console.log(value.length);
