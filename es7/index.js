cdcd9a411cd112d6c1525e657bf39c4587a8c85b
missaelcv <missaelcv0220@gmail.com>
2022-04-28 17:52:55 -0400

    metodo include ES7

---
+++ es7/index.js
@@ -0,0 +1,9 @@
+let numbers = [ 1,2,4,5,7,8];
+
+//Para saber y encontrar si hay un valor en un string o un arreglo
+if (numbers.includes(7)) {
+    console.log('Si encuentro el valor 7')
+} else {
+    console.log('no se encuentra')
+}
+
