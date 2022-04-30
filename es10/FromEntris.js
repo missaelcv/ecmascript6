4208520bd771a4036ac7f1b668c6b01879cc3d04
missaelcv <missaelcv0220@gmail.com>
2022-04-29 23:30:38 -0400

    FromEntris

---
+++ es10/FromEntris.js
@@ -0,0 +1,5 @@
+
+//Tranforma un valor a objeto dfstruturado
+let entries = [["name","Missael"],["agen", 28]];
+console.log(Object.fromEntries(entries));
+//Devuelve un objeto con la clave valos de los elementos
