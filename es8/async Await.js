a4751635f60861136b043b76425c9dfdb0a337ff
missaelcv <missaelcv0220@gmail.com>
2022-05-02 22:41:43 -0400

    Usando el Async

---
+++ es8/async Await.js
@@ -0,0 +1,16 @@
+const helloMundo = () => {
+    return new Promise ((resolve, reject) => {
+    (true)
+    ? setTimeout(() => resolve ('helloMundo') , 3000)
+    : reject(new Error('Text Error'))
+    })
+}
+//Funcion Con Async
+const helloAsync = async () => {
+    //Palabra Reservada 
+    const hello = await helloMundo();
+    const hello2 = await helloMundo();
+    console.log(hello);
+    console.log(hello2);
+}
+helloAsync();
