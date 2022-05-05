bda905bf893650d895a22a0b562581f36b379f68
missaelcv <missaelcv0220@gmail.com>
2022-05-04 23:28:47 -0400

    import en module

--- es11/index.js
+++ es11/index.js
@@ -1,8 +1,8 @@
 //Dynami Import 
 
-const button = document.getElementById("id");
+const button = document.getElementById("btn");
 
 button.addEventListener("click",async function(){
-    const module = await("./file.js");
+    const module = await import ("./file.js");
     module.hello();
 });
