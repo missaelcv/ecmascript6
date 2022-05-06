564e280503cdb18d50dc352eba6409db614a54b3
missaelcv <missaelcv0220@gmail.com>
2022-05-05 22:57:51 -0400

    Metodo Para Replazar palabras string

---
+++ es12/index.js
@@ -0,0 +1,10 @@
+
+//Para replazar 
+const string = "Javascript es maravilloso, con JavaScript puede crear el futuro de la web"
+//Metodo Replace primaria 
+const replaceString = string.replace("Javascript", "Python");
+console.log(replaceString);
+
+//Para Replazar todas las frase que se repiten
+const replaceString2 = string.replaceAll("JavaScript", "Python");
+console.log(replaceString2);
